cfe30e85e8f1b01f987970d948d78b219553e89b
smbirch <spencer.m.birch@gmail.com>
2021-05-21 20:56:14 -0700

    Cleaning up

--- FaceDetection/Program.cs
+++ FaceDetection/Program.cs
@@ -4,10 +4,8 @@ using System.IO;
 using System.Net;
 using System.Collections.Generic;
 
-//using SixLabors.Primitives;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
-using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Drawing.Processing;
 using System.Diagnostics;
 
@@ -18,41 +16,37 @@ namespace FaceDetection
         private static string msg = "Please provide the API key as the first CLI parameter, followed by the filename of the image";
         static void Main(string[] args)
         {
-            //cmd line args
 
             var apiKey = !string.IsNullOrWhiteSpace(args[0]) ? args[0] : throw new ArgumentException(msg, args[0]);
             var filename = File.Exists(args[1]) ? args[1] : throw new FileNotFoundException(msg, args[1]);
 
-            //http request
 
 
             var region = "westus";
             var target = new Uri($"https://{region}.api.cognitive.microsoft.com/face/v1.0/detect/?subscription-key={apiKey}");
             var httpPost = CreateHttpRequest(target, "POST", "application/octet-stream");
 
-            //load image
 
             using (var fs = File.OpenRead(filename))
             {
                 fs.CopyTo(httpPost.GetRequestStream());
             }
 
-            //submit image to API endpoint
 
             string data = getResponse(httpPost);
 
-            //inspect JSON
 
             var rectangles = GetRectangles(data);
 
-            //draw rectangles on the image (copy)
 
             var img = Image.Load(filename);
+
+            OpenWithDefaultApp(filename);
 
             var count = 0;
             foreach (var rectangle in GetRectangles(data))
             {
-                img.Mutate(a => a.DrawPolygon(Color.HotPink, 20, rectangle));
+                img.Mutate(a => a.DrawPolygon(Color.MintCream, 10, rectangle));
                 count++;
             }
             Console.WriteLine($"Number of faces detected: {count}");
